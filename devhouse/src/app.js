2e3eef2a24214175fcd5e328833adb5af8ad1c12
Ctrl-Mota <lucas.d.mota@gmail>
2020-09-16 17:07:35 -0300

    error upload

---
+++ devhouse/src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose'
+import routes from './routes';
+
+
+class App{
+
+    constructor(){
+        this.server = express();
+        mongoose.connect('mongodb+srv://devhouse:motorola123@devhouse.kheh3.mongodb.net/devhouse?retryWrites=true&w=majority',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares(){
+        this.server.use(express.json());
+    }
+    routes(){
+        this.server.use(routes);
+    }
+}
+export default new App().server;
